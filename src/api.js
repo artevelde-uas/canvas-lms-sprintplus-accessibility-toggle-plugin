831d09ced7c6a78acacecf0ca26272980b14a1d7
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2026-09-14 23:51:51 +0200

    fix: Handle the case where there is no data for the given scope

--- src/api.js
+++ src/api.js
@@ -7,7 +7,12 @@ const pkg = require('../package.json');
 export async function getUserData(scope = '') {
     const response = await api.get(`/users/self/custom_data/${scope}`, { ns: pkg.name });
 
-    // If the API response contains a message, it indicates an error, so we throw it as an exception
+    // Handle the case where there is no data for the given scope
+    if (response.message === 'no data for scope') {
+        return null;
+    }
+
+    // If the API response contains any other message, it indicates an error, so we throw it as an exception
     if (response.message !== undefined) {
         throw new Error(response.message);
     }
@@ -18,7 +23,12 @@ export async function getUserData(scope = '') {
 export async function setUserData(scope = '', data) {
     const response = await api.put(`/users/self/custom_data/${scope}`, { data }, { ns: pkg.name });
 
-    // If the API response contains a message, it indicates an error, so we throw it as an exception
+    // Handle the case where there is no data for the given scope
+    if (response.message === 'no data for scope') {
+        return null;
+    }
+
+    // If the API response contains any other message, it indicates an error, so we throw it as an exception
     if (response.message !== undefined) {
         throw new Error(response.message);
     }
